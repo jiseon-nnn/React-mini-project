a741d6b61d6e9bcd61383d71d00e87d6aaf6e12a
jiseon_an <ajsajs1031@gmail.com>
2024-11-06 15:47:19 +0900

    mission1

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
 
   return (
     <>
-
       <Routes>
         <Route path={'/'} element={<Main />} />
         <Route path={'/details'} element={<MovieDetail movie={movieDetail} />} />
